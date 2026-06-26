9f3f5b9807f6c9972f04bdb423612633f4c45709
Ian Mendoza <github3629@gmail.com>
2026-06-26 10:43:57 +0800

    Fix missing cloud_firestore import

--- lib/pages/appointments_page.dart
+++ lib/pages/appointments_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../services/firestore_service.dart';
 import '../theme/app_theme.dart';
 import '../widgets/widgets.dart';
